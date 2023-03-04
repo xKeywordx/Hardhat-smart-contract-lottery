12344af409805944c7c639d94a82a3888c0849bd
Keyword <ptjocuri4@gmail.com>
2023-03-04 12:43:34 +0200

    initial commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,29 @@
+const { ethers } = require("hardhat");
+
+const networkConfig = {
+    31337: {
+        name: "hardhat",
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: ethers.utils.parseEther("0.001"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    5: {
+        name: "goerli",
+        subscriptionId: "9684",
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: ethers.utils.parseEther("0.001"), // 0.001 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+};
